5becd65008f1237ba05130c8bc7ca2da024ddb8e
Robert DeLuca <robertdeluca19@gmail.com>
2019-05-31 09:28:08 -0500

    Run test suite in parallel (#4)
    Also clean up a little bit of the package.json. Require node 8.2 for npx, remove
    `private: false` since that's implied

--- test/index.js
+++ test/index.js
@@ -1,49 +1,47 @@
 const PercyScript = require("../index.js");
 
-(async () => {
-  await PercyScript.run(async (page, percySnapshot) => {
-    await page.setBypassCSP(true);
-    await page.goto("https://sdk-test.percy.dev");
-    await page.waitFor("#cors");
-    await percySnapshot("Percy SDK test page");
+PercyScript.run(async (page, percySnapshot) => {
+  await page.setBypassCSP(true);
+  await page.goto("https://sdk-test.percy.dev");
+  await page.waitFor("#cors");
+  await percySnapshot("Percy SDK test page");
 
-    await page.goto("http://example.com/");
-    await page.waitFor("h1");
-    await percySnapshot("HTTP live site");
+  await page.goto("http://example.com/");
+  await page.waitFor("h1");
+  await percySnapshot("HTTP live site");
+});
+
+// With snapshot options
+PercyScript.run(async (page, percySnapshot) => {
+  let widths = [550, 780];
+
+  await page.setBypassCSP(true);
+  await page.goto("https://sdk-test.percy.dev");
+  await page.waitFor("#cors");
+  await percySnapshot("Percy SDK test page [with snapshot options]", {
+    widths
   });
 
-  // With snapshot options
-  await PercyScript.run(async (page, percySnapshot) => {
-    let widths = [550, 780];
+  await page.goto("http://example.com/");
+  await page.waitFor("h1");
+  await percySnapshot("HTTP live site [with snapshot options]", { widths });
+});
 
-    await page.setBypassCSP(true);
-    await page.goto("https://sdk-test.percy.dev");
-    await page.waitFor("#cors");
-    await percySnapshot("Percy SDK test page [with snapshot options]", {
-      widths
-    });
+// With launcher options
+const launcherOptions = {
+  defaultViewport: {
+    height: 900,
+    width: 1280
+  }
+};
 
-    await page.goto("http://example.com/");
-    await page.waitFor("h1");
-    await percySnapshot("HTTP live site [with snapshot options]", { widths });
-  });
+PercyScript.run(async (page, percySnapshot) => {
+  await page.setBypassCSP(true);
+  await page.goto("https://sdk-test.percy.dev");
+  await page.waitFor("#cors");
+  await percySnapshot("Percy SDK test page [with launch options]");
 
-  // With launcher options
-  const launcherOptions = {
-    defaultViewport: {
-      height: 900,
-      width: 1280
-    }
-  };
-
-  await PercyScript.run(async (page, percySnapshot) => {
-    await page.setBypassCSP(true);
-    await page.goto("https://sdk-test.percy.dev");
-    await page.waitFor("#cors");
-    await percySnapshot("Percy SDK test page [with launch options]");
-
-    await page.goto("http://example.com/");
-    await page.waitFor("h1");
-    await percySnapshot("HTTP live site [with launch options]");
-  }, launcherOptions);
-})();
+  await page.goto("http://example.com/");
+  await page.waitFor("h1");
+  await percySnapshot("HTTP live site [with launch options]");
+}, launcherOptions);
