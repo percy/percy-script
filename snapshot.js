6f6311e71d71c845bf797b5dab47d43c26de61b3
Robert DeLuca <robertdeluca19@gmail.com>
2019-09-12 11:35:04 -0500

    fix: Give PercyScript its own user agent (#8)
    * Give PercyScript its own user agent
    
    This does duplicate most of the puppeteer SDK, but that's okay. Every other SDK
    uses the same exact pattern for extracting the DOM from the test browser and
    then `POST`'ing that back to the agent process. I don't think we're actually
    DRYing anything up.
    
    * Update CI scripts with yarn
    
    * Fix puppeteer var name typo
    
    * Code review feedback

---
+++ snapshot.js
@@ -0,0 +1,60 @@
+const pkg = require("./package.json");
+const puppeteerPkg = require("puppeteer/package.json");
+const {
+  agentJsFilename,
+  isAgentRunning,
+  postSnapshot
+} = require("@percy/agent/dist/utils/sdk-utils");
+
+const CLIENT_INFO = `${pkg.name}/${pkg.version}`;
+const ENV_INFO = `${puppeteerPkg.name}/${puppeteerPkg.version}`;
+
+module.exports = async function percySnapshot(page, name, options) {
+  if (!page) {
+    throw new Error("Puppeteer 'page' object must be provided.");
+  }
+
+  if (!name) {
+    throw new Error("'name' must be provided.");
+  }
+
+  try {
+    await page.addScriptTag({
+      path: agentJsFilename()
+    });
+  } catch (err) {
+    // Certain CSP settings prevent Puppeteer from injecting scripts. See:
+    // https://github.com/GoogleChrome/puppeteer/issues/2644
+    console.log(
+      `[percy] Could not take snapshot named '${name}', maybe due to stringent CSPs. Try page.setBypassCSP(true).`
+    );
+    return;
+  }
+
+  if (!(await isAgentRunning())) {
+    return;
+  }
+
+  let domSnapshot = await page.evaluate(function(name, options) {
+    let percyAgentClient = new PercyAgent({ handleAgentCommunication: false });
+
+    return percyAgentClient.snapshot("not used");
+  });
+
+  await postDomSnapshot(name, domSnapshot, page.url(), options);
+};
+
+async function postDomSnapshot(name, domSnapshot, url, options) {
+  let postSuccess = await postSnapshot({
+    name,
+    url,
+    domSnapshot,
+    clientInfo: CLIENT_INFO,
+    environmentInfo: ENV_INFO,
+    ...options
+  });
+
+  if (!postSuccess) {
+    console.log(`[percy] Error posting snapshot to agent.`);
+  }
+}
